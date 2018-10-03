716f153a3c9ae135a25853b4682690fc8f2feb46
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-03 08:19:55 -0400

    create events form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,21 @@ class EventsController < ApplicationController
 	end 
 
 	def show 
+		@
 	end 
 
 	def new 
+		@user = current_user 
+		@event = Event.new
 	end 
 
 	def create
+		@event = Event.new(event_params)
+		@user = current_user 
+		@event.user_id = @user.id 
+		@event.save 
+
+		render 'events/show'
 	end 
 
 	def edit
@@ -19,5 +28,11 @@ class EventsController < ApplicationController
 
 	def destroy 
 	end
+
+	private 
+	def event_params
+		params.require(:event).permit(:name, :date, :price, :venue, :description, :user_id)
+	end 
+
 
 end
