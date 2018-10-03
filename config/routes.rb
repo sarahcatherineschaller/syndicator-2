c47a4c436f4d6ad7058c5417243e6585e16855f5
Sarah Schaller <sarahcschaller@gmail.com>
2018-10-03 08:06:30 -0400

    add routes for users and nested routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 	root 'users#index'
 
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :user do 
+		resources :events 
+	end
+
+    devise_for :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
